f0a64526fa6b9b51027bdcd78ceab349bc5922f3
Mohammad Tufail Ahmed <tufailahmedkhan@gmail.com>
2024-09-17 12:43:14 +0530

    sampath undone changes

--- src/test/java/com/training/BankingalstromApplicationTests.java
+++ src/test/java/com/training/BankingalstromApplicationTests.java
@@ -1,7 +1,6 @@
 package com.training;
 
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
@@ -10,7 +9,7 @@ class BankingalstromApplicationTests {
 	@Test
 	void contextLoads() {
 		
-		assertTrue(false);
+		
 	}
 
 }
